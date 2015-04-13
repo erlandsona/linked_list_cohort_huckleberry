07a4de68dcce630e026e5a808381fc0a1a64b82b
Austin Erlandson <austin@erlandson.com>
2015-04-13 15:21:53 -0500

    Tests Up and running... starting implementation

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,14 @@
 class LinkedListItem
+  #Look into Comparable...
   include Comparable
+
+  attr_reader :payload
+
+  def initialize(payload)
+    @payload = payload
+  end
+
+  def next_item=
+  end
+
 end
