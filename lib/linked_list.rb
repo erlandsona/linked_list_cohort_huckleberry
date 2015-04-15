9735eb3069f249be865e909e1cba3e3069dccf65
Austin Erlandson <austin@erlandson.com>
2015-04-15 14:06:12 -0500

    Moving along:)

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,24 +1,83 @@
 class LinkedList
 
-  attr_reader :head
-
-  def initialize(first_value=nil)
-    @head = LinkedListItem.new(first_value) if first_value
+  def push(item)
+    if @first_item.nil?
+      @first_item = LinkedListItem.new(item)
+      @last_item = @first_item
+    else
+      @last_item.next_item = LinkedListItem.new(item)
+      @last_item = @last_item.next_item
+    end
   end
 
-  def push(value)
-    if head.nil?
-      head = LinkedListItem.new(value)
+  def get(nth_item)
+    raise IndexError if nth_item < 0 or @first_item.nil?
+    if nth_item == 0
+      @first_item.payload
     else
-      head.next_item = LinkedListItem.new(value)
+      count = 0
+      current_node = @first_item
+      while count < nth_item
+        raise IndexError if current_node.last?
+        current_node = current_node.next_item
+        count += 1
+      end
+      current_node.payload
     end
   end
 
 
-  def get(index)
-    raise IndexError if index < 0
-    index == 0 ? "foo" : index == 1 ? "bar" : "grille"
+  def size
+   0 if @first_item.last?
   end
+
+#  attr_reader :head
+#
+#  def initialize(first_value=nil)
+#    @head = LinkedListItem.new(first_value) if first_value
+#  end
+#
+#  def push(value)
+#    if head.nil?
+#      head = LinkedListItem.new(value)
+#    else
+#      current_node = @head
+#      while current_node.next_item
+#        current_node = current_node.next_item
+#      end
+#      current_node.next_item = LinkedListItem.new(value)
+#    end
+#  end
+#
+#
+#  def get(index)
+#    count = 0
+#    current_node = @head
+#    while count <= index
+#      if count == index
+#        return current_node
+#      else
+#        current_node = current_node.next_item
+#      end
+#      count++
+#    end
+#  end
+#
+#
+# def get(index)
+#    current_index = 0
+#    current_node = @head
+#    if current_node.nil?
+#      current_node = current_node.next_item
+#    end
+#    while current_index < index
+#      return current_node
+#      current_index++
+#      current_node = current_node.next_item
+#    end
+#
+#    index == 0 ? current_node : "bar"
+#  end
 
 #  def remove(value)
 #    if head.value == value
