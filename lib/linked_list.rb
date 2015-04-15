df6195b16a1fa5b7474a662ff44a17fcdd564062
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-04-15 11:29:40 -0500

    Implemented get and push in class

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,26 @@
 class LinkedList
 
+  def get(nth_item)
+    if nth_item == 0
+      @first_item.payload
+    else
+      count = 0
+      current_node = @first_item
+      while count < nth_item
+        current_node = current_node.next_item
+        count = count + 1
+      end
+      current_node.payload
+    end
+  end
+
+  def push(item)
+    if @first_item.nil?
+      @first_item = LinkedListItem.new(item)
+      @last_item = @first_item
+    else
+      @last_item.next_item = LinkedListItem.new(item)
+      @last_item = @last_item.next_item
+    end
+  end
 end
