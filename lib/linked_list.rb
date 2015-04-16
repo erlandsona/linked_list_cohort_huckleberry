87e5499e1f026a66cac816104d2891e9ab503e3a
Austin Erlandson <austin@erlandson.com>
2015-04-15 23:54:36 -0500

    Can't figure out how to pass an index and a value to method as in... [index]=(value) {do stuff...} ??? Tests 12's still not passing:/

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -26,14 +26,46 @@ class LinkedList
     end
   end
 
-  def get(nth_item)
-    raise IndexError if nth_item < 0 or @first_item.nil?
-    if nth_item == 0
+#  def []=(index, value)
+#    raise IndexError if index < 0 or @first_item.nil?
+#    if index == 0
+#      @first_item.payload = value
+#    else
+#      count = 0
+#      current_node = @first_item
+#      while count < index
+#        raise IndexError if current_node.last?
+#        current_node = current_node.next_item
+#        count += 1
+#      end
+#      current_node.payload = value
+#    end
+#  end
+
+  def get(index)
+    raise IndexError if index < 0 or @first_item.nil?
+    if index == 0
       @first_item.payload
     else
       count = 0
       current_node = @first_item
-      while count < nth_item
+      while count < index
+        raise IndexError if current_node.last?
+        current_node = current_node.next_item
+        count += 1
+      end
+      current_node.payload
+    end
+  end
+
+  def [](index)
+    raise IndexError if index < 0 or @first_item.nil?
+    if index == 0
+      @first_item.payload
+    else
+      count = 0
+      current_node = @first_item
+      while count < index
         raise IndexError if current_node.last?
         current_node = current_node.next_item
         count += 1
@@ -78,54 +110,6 @@ class LinkedList
       return string += " |"
     end
   end
-
-#  attr_reader :head
-#
-#  def initialize(first_value=nil)
-#    @head = LinkedListItem.new(first_value) if first_value
-#  end
-#
-#  def push(value)
-#    if head.nil?
-#      head = LinkedListItem.new(value)
-#    else
-#      current_node = @head
-#      while current_node.next_item
-#        current_node = current_node.next_item
-#      end
-#      current_node.next_item = LinkedListItem.new(value)
-#    end
-#  end
-#
-#
-#  def get(index)
-#    count = 0
-#    current_node = @head
-#    while count <= index
-#      if count == index
-#        return current_node
-#      else
-#        current_node = current_node.next_item
-#      end
-#      count++
-#    end
-#  end
-#
-#
-# def get(index)
-#    current_index = 0
-#    current_node = @head
-#    if current_node.nil?
-#      current_node = current_node.next_item
-#    end
-#    while current_index < index
-#      return current_node
-#      current_index++
-#      current_node = current_node.next_item
-#    end
-#
-#    index == 0 ? current_node : "bar"
-#  end
 
 #  def remove(value)
 #    if head.value == value
